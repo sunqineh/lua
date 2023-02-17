83a06c9e224918ce5207779dfdee8caeaab5a805
sunqineh <125699319+sunqineh@users.noreply.github.com>
2023-02-17 20:48:25 +0000

    Pending changes exported from your codespace

---
+++ manual/wait(0.lua
@@ -0,0 +1,37 @@
+wait(0.1)
+local args = {
+    [1] = "naga naga naga ",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+wait(0.1)
+
+local args = {
+    [1] = "naga naga",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "naga naga naga",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.1)
+local args = {
+    [1] = "naga naga naga",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+wait(0.1)
+local args = {
+    [1] = "naga naga naga bl a ck nl ba s", 
+    [2] = "All"
+}
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
